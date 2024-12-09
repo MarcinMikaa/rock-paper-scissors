70e11705e9318675775565c81202a3d8ddbeddf9
Marcin Mika <marcin.mika123@gmail.com>
2024-12-09 20:18:28 +0100

    Add user prompt and validation Implemented function getHumanChoice which take user choice. The function check if input is valid and contains in rock, paper, scissors options. If not, user is prompted to try again.

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function getComputerChoice() {
   const computerChoices = ['rock', 'paper', 'scissors'];
   return getRandomElement(computerChoices);
 }
+
+function getHumanChoice() {
+  const humanChoice = prompt('Please enter your choice: rock, paper or scissors').toLowerCase();
+  if (['rock', 'paper', 'scissors'].includes(humanChoice)) {
+    return humanChoice;
+  } else {
+    alert('Invalid choice, please try again.');
+    return getHumanChoice();
+  }
+}
